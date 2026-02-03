c50a98538e97ceffcadc2b6bf1c930608324950c
Syed Mehdi <syed@bcw.group>
2026-02-03 16:45:02 +0500

    scripts and payload

---
+++ preprod-server-7.0.0/check.sh
@@ -0,0 +1,12 @@
+# create output directory if it doesn't exist
+mkdir -p out
+
+# take URL in argument or use default
+URL=${1:-'http://127.0.0.1:6300'}
+
+curl -X POST "$URL/check" --data-binary '@check-payload.bin' --output out/check-response.bin
+
+echo "Response saved to out/check-response.bin"
+
+#  print hex dump of response
+hexdump -C out/check-response.bin | head -20
